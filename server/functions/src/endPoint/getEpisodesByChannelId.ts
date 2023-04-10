ad74d73b21f6f7358356a18ce7f36ab6e61e027f
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2023-04-10 21:28:04 +0900

    feat: getEpisodesByChannelId

---
+++ server/functions/src/endPoint/getEpisodesByChannelId.ts
@@ -0,0 +1,57 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { CHANNEL_DOCUMENT_NAME, EPISODE_DOCUMENT_NAME } from '../constans';
+
+const PAGE_SIZE = 30;
+
+export const getEpisodesByChannelId = functions
+  .region('asia-northeast1')
+  .https.onCall(async (data, _) => {
+    // if (request.app == null) {
+    //   throw new functions.https.HttpsError(
+    //     'failed-precondition',
+    //     'The function must be called from an App Check verified app.'
+    //   );
+    // }
+    const channelId = data.channelId;
+    const page = data.page || 1;
+
+    if (!channelId) {
+      throw new functions.https.HttpsError(
+        'invalid-argument',
+        'The function must be called with a "channelId" argument.'
+      );
+    }
+
+    const channelRef = admin.firestore().collection(CHANNEL_DOCUMENT_NAME).doc(channelId);
+    const channelDoc = await channelRef.get();
+
+    if (!channelDoc.exists) {
+      throw new functions.https.HttpsError('not-found', 'The specified channel does not exist.');
+    }
+
+    const offset = (page - 1) * PAGE_SIZE;
+    const episodesSnapshot = await channelRef
+      .collection(EPISODE_DOCUMENT_NAME)
+      .orderBy('pubDate', 'desc')
+      .offset(offset)
+      .limit(PAGE_SIZE)
+      .get();
+
+    const episodesData = episodesSnapshot.docs.map((doc) => {
+      const data = doc.data();
+      return {
+        id: doc.id,
+        title: data.title,
+        description: data.description,
+        url: data.url,
+        content: data.content,
+        duration: data.duration,
+        pubDate: data.pubDate,
+        season: data.season,
+        episode: data.episode,
+      };
+    });
+
+    return episodesData;
+  });
--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -2,10 +2,10 @@ import * as admin from 'firebase-admin';
 import { registerChannel } from './endPoint/registerChannel';
 import { getChannels } from './endPoint/getChannels';
 import { getChannelById } from './endPoint/getChannelById';
-
+import { getEpisodesByChannelId } from './endPoint/getEpisodesByChannelId';
 admin.initializeApp();
 admin.firestore().settings({
   ignoreUndefinedProperties: true,
 });
 
-export { registerChannel, getChannels, getChannelById };
+export { registerChannel, getChannels, getChannelById, getEpisodesByChannelId };
