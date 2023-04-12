73ee84f0c9ae95d183e1dfee4229193b8c5692c8
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2023-04-13 00:59:10 +0900

    fix: リダイレクトを

--- front/app/index.tsx
+++ front/app/index.tsx
@@ -1,10 +1,12 @@
-import { useRouter } from "expo-router";
+import { usePathname, useRouter } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import TrackPlayer, { AppKilledPlaybackBehavior, Capability } from "react-native-track-player";
 import { useDidMount } from "../feature/hooks/useDidMount";
+import { useEffect } from "react";
 
 export default function Root() {
   const router = useRouter();
+  const pathname = usePathname()
   const setup = async () => {
     await TrackPlayer.setupPlayer()
     await TrackPlayer.updateOptions({
@@ -24,6 +26,13 @@ export default function Root() {
   useDidMount(() => {
     setup()
   });
+
+  useEffect(() => {
+    if (pathname === '/') {
+      router.replace("/mainTab/home");
+    }
+  }, [pathname, router])
+
   return <>
     <StatusBar style="auto" />
   </>
