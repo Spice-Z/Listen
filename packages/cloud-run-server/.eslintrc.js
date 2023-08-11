c956c12a787b03baee9ff9dc73bba1f1e30ac8d3
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2023-08-11 17:02:49 +0900

    cloud-run-server

---
+++ packages/cloud-run-server/.eslintrc.js
@@ -0,0 +1,15 @@
+const base = require.resolve('@listen/eslint-config/base');
+
+module.exports = {
+  root: true,
+  extends: [base],
+  env: {
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    project: 'tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+};
