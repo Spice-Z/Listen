0fb9f97ab929845399cd1e2f59c1b24a5df8aea5
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2023-08-16 15:57:20 +0900

    NODE_ENVによるデフォルトの挙動に任せる

--- packages/graphql-server/src/index.ts
+++ packages/graphql-server/src/index.ts
@@ -4,9 +4,7 @@ import {resolvers} from './resolvers/index.js';
 import { readFileSync } from 'node:fs'
 
 import { dirname, join } from 'path'
-import { ApolloServerPluginLandingPageDisabled } from '@apollo/server/plugin/disabled';
 
-const isDevelopment = process.env.NODE_ENV === 'development'
 const port = process.env.PORT || 8080
 
 const filePath = join(dirname(new URL(import.meta.url).pathname), '../generated/schema.graphql')
@@ -16,7 +14,6 @@ const typeDefs = readFileSync(filePath, 'utf8')
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  plugins: isDevelopment ? [ApolloServerPluginLandingPageDisabled()] : [],
 });
 
 const { url } = await startStandaloneServer(server, {
