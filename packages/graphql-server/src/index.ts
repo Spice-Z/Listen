34c72b76930aa77312ccf2fb609a79760293583b
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2023-08-16 15:48:18 +0900

    apollo studio出し訳

--- packages/graphql-server/src/index.ts
+++ packages/graphql-server/src/index.ts
@@ -4,17 +4,23 @@ import {resolvers} from './resolvers/index.js';
 import { readFileSync } from 'node:fs'
 
 import { dirname, join } from 'path'
+import { ApolloServerPluginLandingPageDisabled } from '@apollo/server/plugin/disabled';
+
+const isDevelopment = process.env.NODE_ENV === 'development'
+const port = process.env.PORT || 8080
 
 const filePath = join(dirname(new URL(import.meta.url).pathname), '../generated/schema.graphql')
 const typeDefs = readFileSync(filePath, 'utf8')
 
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: isDevelopment ? [ApolloServerPluginLandingPageDisabled()] : [],
 });
 
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 8080 },
+  listen: { port: Number(port) },
 });
 
 console.log(`🚀  Server ready at: ${url}`);
