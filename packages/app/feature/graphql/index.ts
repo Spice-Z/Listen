62d8e1161a855d565d3114bbc27f04bf68bb8b16
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2023-08-20 13:37:40 +0900

    api url

--- packages/app/feature/graphql/index.ts
+++ packages/app/feature/graphql/index.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'aaa',
+  uri: 'https://api.listen-world.com',
   cache: new InMemoryCache(),
 });
