370e111e1020cf653c44e115778a080ca35a922b
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2023-08-23 12:03:39 +0900

    ログイン画面のスタイル

--- packages/app/app/signIn.tsx
+++ packages/app/app/signIn.tsx
@@ -2,7 +2,7 @@ import { memo } from "react";
 
 import AppleSignInButton from "../feature/Auth/AppleSignInButton";
 import { theme } from "../feature/styles/theme";
-import { SafeAreaView, StyleSheet } from "react-native";
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { Stack } from "expo-router";
 
 const SignIn = memo(() => {
@@ -11,7 +11,13 @@ const SignIn = memo(() => {
    headerShown: false,
   }} />
   <SafeAreaView style={styles.container}>
-   <AppleSignInButton />
+   <Image width={2292} height={2292} style={styles.image} source={require('../assets/image/login-artwork.png')} />
+   <Text style={styles.text}>{`Listen English Podcast\nwith your language transcript`}</Text>
+   <View style={styles.buttonContainer}>
+    <AppleSignInButton />
+    <View style={{ height: 16 }} />
+    <AppleSignInButton />
+   </View>
   </SafeAreaView >
  </>
 })
@@ -22,6 +28,26 @@ export default SignIn;
 const styles = StyleSheet.create({
  container: {
   flex: 1,
-  backgroundColor: theme.color.bgMain,
+  backgroundColor: theme.color.bgEmphasis,
+  width: '100%',
+  alignItems: 'center',
+  justifyContent: 'space-around',
+  flexDirection: 'column',
  },
+ image: {
+  width: '90%',
+  height: 'auto',
+  aspectRatio: 1,
+  resizeMode: 'contain',
+ },
+ text: {
+  fontSize: 22,
+  fontWeight: '600',
+  color: theme.color.textMain,
+  textAlign: 'center',
+ },
+ buttonContainer: {
+  width: '100%',
+  alignItems: 'center',
+ }
 });
