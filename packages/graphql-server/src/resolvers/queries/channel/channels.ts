587d5bddae452fdc5b1a8d1db018706642ee97e1
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2023-09-13 20:55:58 +0900

    fix: channel order

--- packages/graphql-server/src/resolvers/queries/channel/channels.ts
+++ packages/graphql-server/src/resolvers/queries/channel/channels.ts
@@ -24,6 +24,7 @@ const resolver: QueryResolvers['channels'] = async (parent, args, context, info)
     .collection(CHANNEL_DOCUMENT_NAME)
     .withConverter(channelConverter)
     .limit(first)
+    .orderBy('updatedAt', 'desc')
     .get();
   const edges = snapshot.docs.map((doc) => {
     const data = doc.data();
