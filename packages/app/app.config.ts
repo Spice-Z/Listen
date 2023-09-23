67d5bfc90d990b7b39a6f30bc18d72c99482520b
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2023-09-23 18:49:07 +0900

    script: dev/prod

--- packages/app/app.config.ts
+++ packages/app/app.config.ts
@@ -1,17 +1,20 @@
 import { ExpoConfig, ConfigContext } from 'expo/config';
 
-const isProd = process.env.NODE_ENV === 'production';
+const IS_PROD = process.env.APP_VARIANT === 'production';
 
 export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
   return {
     ...config,
+    name: IS_PROD ? 'Listen' : '[dev]Listen',
     ios: {
       ...config.ios,
-      googleServicesFile: isProd ? './GoogleService-Info.plist' : './GoogleService-Info-Dev.plist',
+      googleServicesFile: IS_PROD ? './GoogleService-Info.plist' : './GoogleService-Info-Dev.plist',
+      bundleIdentifier: IS_PROD ? 'com.spicex.Listen' : 'com.spicex.Listen.dev',
     },
     android: {
       ...config.android,
-      googleServicesFile: isProd ? './google-services.json' : './google-services-dev.json',
+      googleServicesFile: IS_PROD ? './google-services.json' : './google-services-dev.json',
+      package: IS_PROD ? 'com.spicex.Listen' : 'com.spicex.Listen.dev',
     },
   };
 };
