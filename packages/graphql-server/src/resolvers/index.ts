681c8af88591f2e6eaafc8969bd2d43082d6560f
Spice-Z <15419227+Spice-Z@users.noreply.github.com>
2023-11-19 18:07:31 +0900

    feat: import忘れ

--- packages/graphql-server/src/resolvers/index.ts
+++ packages/graphql-server/src/resolvers/index.ts
@@ -3,6 +3,8 @@ import channelsResolvers from './queries/channel/channels.js';
 import episodeResolvers from './queries/episode/episode.js';
 import nodeResolvers from './queries/node.js';
 import channelEpisodesResolvers from './queries/channel/episodes.js';
+import allEpisodesResolvers from './queries/episode/allEpisodes.js';
+import episodeChannelResolvers from './queries/episode/channel.js';
 import type { Resolvers } from '../../generated/resolvers-types.js';
 
 export const resolvers: Resolvers = {
@@ -11,6 +13,8 @@ export const resolvers: Resolvers = {
     ...channelsResolvers.resolvers,
     ...episodeResolvers.resolvers,
     ...nodeResolvers.resolvers,
+    ...allEpisodesResolvers.resolvers,
   },
   ...channelEpisodesResolvers.resolvers,
+  ...episodeChannelResolvers.resolvers,
 };
